adaad17f29a0c9c6229d6393a38d3d8de3eda21d
Viktor Persson <viktor.persson@arcsin.se>
2024-04-29 12:43:19 +0200

    matchdate: Add method to get matchdate as string (in addition to datetime).

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub use crate::ktxstats::ktxstats;
-pub use crate::matchdate::matchdate;
+pub use crate::matchdate::{matchdate, matchdate_str};
 pub use crate::serverinfo::serverinfo;
 
 mod frame;
--- src/matchdate.rs
+++ src/matchdate.rs
@@ -1,6 +1,7 @@
-use crate::timezone;
 use bstr::ByteSlice;
 use chrono::{DateTime, Utc};
+
+use crate::timezone;
 
 const NEEDLE: &[u8; 11] = b"matchdate: ";
 const DATETIME_LEN: usize = "yyyy-mm-dd hh:mm:ss".len();
@@ -8,6 +9,21 @@ const MIN_LEN: usize = "yyyy-mm-dd hh:mm:ss ab".len();
 const MAX_LEN: usize = "yyyy-mm-dd hh:mm:ss abcde".len();
 
 pub fn matchdate(data: &[u8]) -> Option<DateTime<Utc>> {
+    let matchdate_str = replace_tz_abbr_with_offset(&matchdate_str(data)?)?;
+
+    match DateTime::parse_from_str(&matchdate_str, "%Y-%m-%d %H:%M:%S%z") {
+        Ok(dt) => Some(dt.to_utc()),
+        Err(_) => None,
+    }
+}
+
+pub fn replace_tz_abbr_with_offset(timestamp: &str) -> Option<String> {
+    let tz_abbr = &timestamp[DATETIME_LEN + 1..];
+    let tz_offset = timezone::utc_offset(tz_abbr)?;
+    Some(format!("{}{}", &timestamp[..DATETIME_LEN], tz_offset))
+}
+
+pub fn matchdate_str(data: &[u8]) -> Option<String> {
     let index_from = data.find(NEEDLE)? + NEEDLE.len();
     let index_to = index_from + data[index_from..].find_byte(b'\n')?;
     let length = index_to - index_from;
@@ -16,19 +32,7 @@ pub fn matchdate(data: &[u8]) -> Option<DateTime<Utc>> {
         return None;
     }
 
-    let Ok(matchdate_with_tz_abbr) = String::from_utf8(data[index_from..index_to].to_vec()) else {
-        return None;
-    };
-
-    let matchdate_with_tz_offset = {
-        let tz_abbr = &matchdate_with_tz_abbr[DATETIME_LEN + 1..];
-        matchdate_with_tz_abbr.replace(tz_abbr, &timezone::utc_offset(tz_abbr)?)
-    };
-
-    match DateTime::parse_from_str(&matchdate_with_tz_offset, "%Y-%m-%d %H:%M:%S%z") {
-        Ok(dt) => Some(dt.to_utc()),
-        Err(_) => None,
-    }
+    String::from_utf8(data[index_from..index_to].to_vec()).ok()
 }
 
 #[cfg(test)]
@@ -39,52 +43,77 @@ mod tests {
 
     use super::*;
 
-    fn to_utc_opt(value: &str) -> Option<DateTime<Utc>> {
-        Some(DateTime::parse_from_rfc3339(value).unwrap().to_utc())
+    #[test]
+    fn test_replace_tz_abbr_with_offset() {
+        assert_eq!(
+            replace_tz_abbr_with_offset("2024-04-02 21:02:17 CEST"),
+            Some("2024-04-02 21:02:17+02:00".to_string())
+        );
+        assert_eq!(
+            replace_tz_abbr_with_offset("2024-04-02 21:02:17 GMT"),
+            Some("2024-04-02 21:02:17+00:00".to_string())
+        );
+        assert_eq!(
+            replace_tz_abbr_with_offset("2024-04-02 21:02:17 UTC"),
+            Some("2024-04-02 21:02:17+00:00".to_string())
+        );
+        assert_eq!(
+            replace_tz_abbr_with_offset("2024-04-02 21:02:17 -01"),
+            Some("2024-04-02 21:02:17-01:00".to_string())
+        );
+        assert_eq!(
+            replace_tz_abbr_with_offset("2024-04-02 21:02:17 +0200"),
+            Some("2024-04-02 21:02:17+02:00".to_string())
+        );
     }
 
     #[test]
     fn test_matchdate() -> Result<()> {
+        assert_eq!(
+            matchdate(b"matchdate: 2024-04-02 21:02:17 CEST\n"),
+            Some(
+                DateTime::parse_from_rfc3339("2024-04-02T19:02:17+00:00")
+                    .unwrap()
+                    .to_utc()
+            )
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_matchdate_str() -> Result<()> {
         // invalid
-        assert_eq!(matchdate(b""), None);
-        assert_eq!(matchdate(b"foo"), None);
-        assert_eq!(matchdate(b"matchdate: foo"), None);
-        assert_eq!(matchdate(b"matchdate: 2024"), None);
-        assert_eq!(matchdate(b"matchdate: 2024-04-02 21:02:17\n"), None);
-        assert_eq!(matchdate(b"matchdate: 2024-04-02 21:02:17 FOOBAR\n"), None);
+        assert_eq!(matchdate_str(b""), None);
+        assert_eq!(matchdate_str(b"foo"), None);
+        assert_eq!(matchdate_str(b"matchdate: foo"), None);
+        assert_eq!(matchdate_str(b"matchdate: 2024"), None);
+        assert_eq!(matchdate_str(b"matchdate: 2024-04-02 21:02:17\n"), None);
+        assert_eq!(
+            matchdate_str(b"matchdate: 2024-04-02 21:02:17 FOOBAR\n"),
+            None
+        );
 
         // valid
         assert_eq!(
-            matchdate(b"matchdate: 2024-04-02 21:02:17 CEST\n"),
-            to_utc_opt("2024-04-02T19:02:17+00:00")
-        );
-        assert_eq!(
-            matchdate(b"matchdate: 2024-04-02 21:02:17 GMT\n"),
-            to_utc_opt("2024-04-02T21:02:17+00:00")
-        );
-        assert_eq!(
-            matchdate(b"matchdate: 2024-04-02 21:02:17 UTC\n"),
-            to_utc_opt("2024-04-02T21:02:17+00:00")
-        );
-        assert_eq!(
-            matchdate(b"matchdate: 2024-04-02 21:02:17 -01\n"),
-            to_utc_opt("2024-04-02T22:02:17+00:00")
-        );
-        assert_eq!(
-            matchdate(b"matchdate: 2024-04-02 21:02:17 +0200\n"),
-            to_utc_opt("2024-04-02T19:02:17+00:00")
+            matchdate_str(b"matchdate: 2024-04-02 21:02:17 CEST\n"),
+            Some("2024-04-02 21:02:17 CEST".to_string())
         );
 
         // files
-        {
-            let data = std::fs::read("tests/files/duel_holy_vs_dago[bravado]20240426-1659.mvd")?;
-            assert_eq!(matchdate(&data), to_utc_opt("2024-04-26 16:59:29+02:00"))
-        }
+        assert_eq!(
+            matchdate_str(&std::fs::read(
+                "tests/files/duel_holy_vs_dago[bravado]20240426-1659.mvd"
+            )?),
+            Some("2024-04-26 16:59:29 CEST".to_string())
+        );
 
-        {
-            let data = std::fs::read("tests/files/4on4_oeks_vs_tsq[dm2]20240426-1716.mvd")?;
-            assert_eq!(matchdate(&data), to_utc_opt("2024-04-26 17:16:13+02:00"));
-        }
+        assert_eq!(
+            matchdate_str(&std::fs::read(
+                "tests/files/4on4_oeks_vs_tsq[dm2]20240426-1716.mvd"
+            )?),
+            Some("2024-04-26 17:16:13 CEST".to_string())
+        );
 
         Ok(())
     }
