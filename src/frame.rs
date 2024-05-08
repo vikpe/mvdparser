d148ed4834c006b89512245800ed967cd819114b
Viktor Persson <viktor.persson@arcsin.se>
2024-05-08 10:46:18 +0200

    Add detailed frame info.

--- src/frame.rs
+++ src/frame.rs
@@ -1,4 +1,6 @@
-use anyhow::anyhow as e;
+use std::ops::Range;
+
+use anyhow::{anyhow as e, Result};
 
 use crate::num;
 use crate::qw::{Command, Target};
@@ -13,7 +15,7 @@ mod index {
     pub const SIZE: usize = 2;
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Info {
     pub duration: u32,
     pub target: Target,
@@ -59,6 +61,78 @@ impl TryFrom<&[u8]> for Info {
     }
 }
 
+#[derive(Clone, Debug, PartialEq)]
+pub struct DetailedInfo {
+    pub duration: u32,
+    pub target: Target,
+    pub command: Command,
+    pub header: SliceInfo,
+    pub body: SliceInfo,
+    pub size: usize,
+    pub range_abs: Range<usize>,
+    pub range_rel: Range<usize>,
+}
+
+impl DetailedInfo {
+    pub fn from_data_and_index(source_data: &[u8], index: usize) -> Result<Self> {
+        let data = &source_data[index..];
+
+        if data.len() < MULTI_HEADER_SIZE {
+            return Err(e!("frame::Info: unsufficient length"));
+        }
+
+        let target = Target::from(&data[index::TARGET]);
+        let command = Command::from(&data[index::COMMAND]);
+
+        // skip multi target bytes [0,0,0,0]
+        let skipped_bytes = match target {
+            Target::Multiple => num::size::LONG,
+            _ => 0,
+        };
+
+        // header
+        let header_size = HEADER_SIZE + skipped_bytes;
+        let header = SliceInfo {
+            size: header_size,
+            range_abs: index..index + header_size,
+            range_rel: 0..header_size,
+        };
+
+        // body
+        let body_size = match command {
+            Command::Read => num::long(&data[skipped_bytes + index::SIZE..]) as usize,
+            Command::Set => 2 * num::size::LONG, // reads 2 longs (8 bytes)
+            _ => 0,                              // should not happen
+        };
+
+        let body = SliceInfo {
+            size: body_size,
+            range_abs: index + header_size..index + header_size + body_size,
+            range_rel: header_size..header_size + body_size,
+        };
+
+        // frame
+        let frame_size = header.size + body.size;
+        Ok(Self {
+            duration: data[index::DURATION] as u32,
+            target,
+            command,
+            header,
+            body,
+            size: frame_size,
+            range_abs: index..index + frame_size,
+            range_rel: 0..frame_size,
+        })
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct SliceInfo {
+    pub size: usize,
+    pub range_abs: Range<usize>,
+    pub range_rel: Range<usize>,
+}
+
 #[cfg(test)]
 mod tests {
     use std::fs::read;
@@ -69,7 +143,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_frameinfo() -> Result<()> {
+    fn test_info() -> Result<()> {
         let data = read("tests/files/4on4_oeks_vs_tsq[dm2]20240426-1716.mvd")?;
 
         assert_eq!(
@@ -93,6 +167,35 @@ mod tests {
                 header_size: 6,
                 body_size: 737,
                 total_size: 743,
+            }
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_detailed_info() -> Result<()> {
+        let data = read("tests/files/4on4_oeks_vs_tsq[dm2]20240426-1716.mvd")?;
+
+        assert_eq!(
+            DetailedInfo::from_data_and_index(&data, 456)?,
+            DetailedInfo {
+                duration: 0,
+                target: Target::All,
+                command: Command::Read,
+                header: SliceInfo {
+                    size: 6,
+                    range_abs: 456..462,
+                    range_rel: 0..6,
+                },
+                body: SliceInfo {
+                    size: 737,
+                    range_abs: 462..1199,
+                    range_rel: 6..743,
+                },
+                size: 743,
+                range_abs: 456..1199,
+                range_rel: 0..743,
             }
         );
 
