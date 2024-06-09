e757072ddd53987df5d364e9dd760296a3701e9a
Viktor Persson <viktor.persson@arcsin.se>
2024-06-09 17:05:06 +0200

    Add functions to parse player/spectator clients.

--- src/clients.rs
+++ src/clients.rs
@@ -16,6 +16,24 @@ pub fn clients(data: &[u8]) -> Result<Vec<Client>> {
     Ok(clients)
 }
 
+pub fn player_clients(data: &[u8]) -> Result<Vec<Client>> {
+    let players = clients(data)?
+        .iter()
+        .filter(|c| !c.is_spectator)
+        .cloned()
+        .collect();
+    Ok(players)
+}
+
+pub fn spectator_clients(data: &[u8]) -> Result<Vec<Client>> {
+    let spectators = clients(data)?
+        .iter()
+        .filter(|c| c.is_spectator)
+        .cloned()
+        .collect();
+    Ok(spectators)
+}
+
 #[cfg(test)]
 mod tests {
     use std::fs::read;
@@ -26,7 +44,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_clientinfo() -> Result<()> {
+    fn test_clients() -> Result<()> {
         assert_eq!(
             clients(&read(
                 "tests/files/duel_equ_vs_kaboom[povdmm4]20240422-1038.mvd"
@@ -57,6 +75,54 @@ mod tests {
                     is_bot: false,
                 },
             ]
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_player_clients() -> Result<()> {
+        assert_eq!(
+            player_clients(&read(
+                "tests/files/duel_equ_vs_kaboom[povdmm4]20240422-1038.mvd"
+            )?)?,
+            vec![
+                Client {
+                    number: 0,
+                    name: "eQu".to_string(),
+                    team: "red".to_string(),
+                    color: [4, 4],
+                    is_spectator: false,
+                    is_bot: false,
+                },
+                Client {
+                    number: 2,
+                    name: "KabÏÏm".to_string(),
+                    team: "".to_string(),
+                    color: [2, 2],
+                    is_spectator: false,
+                    is_bot: false,
+                },
+            ]
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_spectator_clients() -> Result<()> {
+        assert_eq!(
+            spectator_clients(&read(
+                "tests/files/duel_equ_vs_kaboom[povdmm4]20240422-1038.mvd"
+            )?)?,
+            vec![Client {
+                number: 1,
+                name: "[ServeMe]".to_string(),
+                team: "lqwc".to_string(),
+                color: [12, 11],
+                is_spectator: true,
+                is_bot: false,
+            },]
         );
 
         Ok(())
